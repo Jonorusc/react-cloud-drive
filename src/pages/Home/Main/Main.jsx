8ee67c2b2c48507e8f506e448476b887453a0686
João Lucas <joaolucascp16@gmail.com>
2022-06-14 00:39:50 -0300

    react filesystem structure

---
+++ src/pages/Home/Main/Main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainContent from './MainContent'
+
+import './Main.css'
+
+function Main() {
+    return (
+        <main>
+            <MainContent />
+        </main>
+    )
+}
+
+export default Main
