fd0f4918dab3f639a8b91bad6494af1d1cbe5d54
João Lucas <joaolucascp16@gmail.com>
2022-06-15 19:29:21 -0300

    update userReducer.js: getting the state by cookies

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,4 +1,9 @@
-export function userReducer(state = 'jao', action) {
+import Cookies from 'js-cookie'
+
+export function userReducer(
+    state = Cookies.get('user') ? JSON.parse(Cookies.get('user')) : null,
+    action
+) {
     switch(action.type) {
         case 'LOGIN' :
             return action.payload
