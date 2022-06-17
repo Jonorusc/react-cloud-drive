a749d520e85e691d65beb1f9dffc3d16a9548854
João Lucas <joaolucascp16@gmail.com>
2022-06-17 17:28:16 -0300

    User.jsx: close modal on click out

--- src/pages/Home/Aside/User.jsx
+++ src/pages/Home/Aside/User.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import Avatar from '@mui/material/Avatar' 
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
 import AsideOption from '../Aside/AsideOption'
@@ -7,17 +7,26 @@ import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded'
 
 // import userProfile from '../../../images/WhatsApp Image 2022-04-27 at 13.26.56.jpeg'
 import './User.css'
+import useClickOutside from '../../../helpers/clickOutside'
 
 function User({ name, picture }) {
-    const [modal, openModal] = useState(false)
+    const [modal, openModal] = useState(false),
+        modalRef = useRef()
 
     function handleModal() {
         modal ? openModal(false) : openModal(true)
     }
 
+    useClickOutside(modalRef, () => {
+        openModal(false)
+    })
+
     function Modal() {
         return (
-            <div className={modal ? "user_modal active" : "user_modal"} >
+            <div 
+                className={modal ? "user_modal active" : "user_modal"} 
+                ref={modalRef}
+            >
                 <AsideOption Icon={AccountCircleRoundedIcon} option='My Profile' active={false}/>
                 <AsideOption Icon={LogoutRoundedIcon} option='Log-out' active={false}/>
             </div>
