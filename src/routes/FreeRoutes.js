b7fca65f899ac1d88f3bd4dbd69898fbfcebd58c
João Lucas <joaolucascp16@gmail.com>
2022-06-17 17:15:01 -0300

    checking Free Routes and Registered Routes

---
+++ src/routes/FreeRoutes.js
@@ -0,0 +1,7 @@
+import { useSelector } from "react-redux"
+import { Navigate, Outlet } from "react-router-dom"
+
+export default function FreeRoutes() {
+    const { user } = useSelector((state) => ({ ...state }))
+    return user ? <Navigate to="/" /> : <Outlet />
+}
