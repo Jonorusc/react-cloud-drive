fb45708b793539d788ddf83cba443aaf61cd0ee8
João Lucas <joaolucascp16@gmail.com>
2022-06-17 17:14:19 -0300

    Header.jsx: refactored

--- src/pages/Home/Header/Header.jsx
+++ src/pages/Home/Header/Header.jsx
@@ -1,22 +1,14 @@
 import React, { useRef } from 'react'
 import AddToDriveIcon from '@mui/icons-material/AddToDrive'
 import SearchIcon from '@mui/icons-material/Search'
-// import MenuRoundedIcon from '@mui/icons-material/MenuRounded'
 
 import './Header.css'
 
-function Header({ hideAside }) {
-    const inputRef = useRef(),
-        toggle = useRef()
+function Header({ hidesidebar, setHideSdebar }) {
+    const inputRef = useRef()
 
     function showInput(){
         inputRef.current.classList.toggle('active')
-    }
-
-    function toogleSidebar() {
-        toggle.current.classList.toggle('active')
-        
-        !toggle.current.classList.contains('active') ? hideAside(false) : hideAside(true)
     }
 
     return (
@@ -36,10 +28,7 @@ function Header({ hideAside }) {
                         placeholder='search for all' 
                         className="header_input" 
                     />
-                    {/* <MenuRoundedIcon className='header_menu'
-                        onClick={hideSidebar}
-                    />  */}
-                    <div ref={toggle} className="header_menu" onClick={toogleSidebar}>
+                    <div className={`header_menu ${hidesidebar ? 'active' : ''}`} onClick={() => {setHideSdebar(hidesidebar => !hidesidebar)}}>
                         <div className="line one"></div>
                         <div className="line two"></div>
                         <div className="line three"></div>
