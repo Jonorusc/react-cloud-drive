14986ada92429ca6dde0e00cfd976c3557270f08
João Lucas <joaolucascp16@gmail.com>
2022-06-19 23:28:33 -0300

    AsideOptions: improving the code

--- src/pages/Home/Aside/AsideOption.jsx
+++ src/pages/Home/Aside/AsideOption.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 import './AsideOption.css'
 
-function AsideOptions( {Icon, option, active} ) {
+function AsideOptions( {Icon, option, active, to} ) {
 
     function asideClass() {
         return active === true ? 'aside_options active' : 'aside_options'
     }
 
     return (
-        <div className={asideClass()} onClick={() => {console.log('action')}}>
+        <Link className={asideClass()} to={ to ? to : '#'}>
             {Icon && <Icon />}
             <span className="option">{option}</span>
-        </div>
+        </Link>
     )
 }
 
