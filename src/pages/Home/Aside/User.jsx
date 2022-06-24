ada7d431d6ceecf5b4117f7940987dd095a4c480
João Lucas <joaolucascp16@gmail.com>
2022-06-23 22:41:59 -0300

    Userjsx: improving the code

--- src/pages/Home/Aside/User.jsx
+++ src/pages/Home/Aside/User.jsx
@@ -1,16 +1,20 @@
 import React, { useState, useRef } from 'react'
-import Avatar from '@mui/material/Avatar' 
+import Avatar from '@mui/material/Avatar'
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown'
-import AsideOption from '../Aside/AsideOption'
+// import AsideOption from '../Aside/AsideOption'
 import AccountCircleRoundedIcon from '@mui/icons-material/AccountCircleRounded'
 import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded'
 import useClickOutside from '../../../helpers/clickOutside'
+import { useDispatch } from 'react-redux'
+import Cookies from 'js-cookie'
+import { Link } from 'react-router-dom'
 
 import './User.css'
 
 function User({ name, picture }) {
     const [modal, openModal] = useState(false),
-        modalRef = useRef()
+        modalRef = useRef(),
+        dispatch = useDispatch()
 
     function handleModal() {
         modal ? openModal(false) : openModal(true)
@@ -20,29 +24,54 @@ function User({ name, picture }) {
         openModal(false)
     })
 
+    function logout() {
+        Cookies.set('user', '')
+        dispatch({
+            type: 'LOGOUT',
+        })
+        window.location.href = '/login'
+    }
+
     function Modal() {
         return (
-            <div 
-                className={modal ? "user_modal active" : "user_modal"} 
-            >
-                <AsideOption Icon={AccountCircleRoundedIcon} option='My Profile' active={false} to="/profile"/>
-                <AsideOption Icon={LogoutRoundedIcon} option='Log-out' active={false}/>
+            <div className={modal ? 'user_modal active' : 'user_modal'}>
+                <Link className="aside_options" to="/profile">
+                    <AccountCircleRoundedIcon />
+                    <span className="option">My Profile</span>
+                </Link>
+                <Link
+                    className="aside_options"
+                    to="#logout"
+                    onClick={() => {
+                        logout()
+                    }}
+                >
+                    <LogoutRoundedIcon />
+                    <span className="option">Logout</span>
+                </Link>
             </div>
         )
     }
 
     return (
-        <div 
-            className='user' 
-            onClick={() => {handleModal()}}
+        <div
+            className="user"
+            onClick={() => {
+                handleModal()
+            }}
             ref={modalRef}
         >
-            <Avatar sx={{
-                width: 40, height: 40}} 
+            <Avatar
+                sx={{
+                    width: 40,
+                    height: 40,
+                }}
                 src={picture}
             />
             <span className="user_name">{name}</span>
-            <ArrowDropDownIcon sx={{transform: modal ? 'rotate(180deg)' : ''}}/>
+            <ArrowDropDownIcon
+                sx={{ transform: modal ? 'rotate(180deg)' : '' }}
+            />
 
             <Modal />
         </div>
