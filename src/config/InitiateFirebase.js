d7f495acbacef0ce711e0add053c22d94f550938
João Lucas <joaolucascp16@gmail.com>
2022-07-04 15:39:37 -0300

    InitiateFirebase.js: getting storage firebase

--- src/config/InitiateFirebase.js
+++ src/config/InitiateFirebase.js
@@ -1,12 +1,25 @@
 import { initializeApp } from 'firebase/app'
 import { getDatabase } from 'firebase/database'
+import { getStorage } from "firebase/storage"
 
-const InitiateFirebase = () => {
+const InitiateFirebase = (which = 'realtime') => {
     const firebaseConfig = {
-        // your project firebase settings
+        apiKey: process.env.REACT_APP_APIKEY,
+        authDomain: 'cloud-drive-e1991.firebaseapp.com',
+        databaseURL: 'https://cloud-drive-e1991-default-rtdb.firebaseio.com',
+        projectId: 'cloud-drive-e1991',
+        storageBucket: 'cloud-drive-e1991.appspot.com',
+        messagingSenderId: '652680835499',
+        appId: '1:652680835499:web:80bc65e74ce48657d822ee',
     },
     app = initializeApp(firebaseConfig)
-    return getDatabase(app)
+    switch(which) {
+        case 'storage': 
+            return getStorage(app)
+        case 'realtime': 
+            return getDatabase(app)
+        default: return 
+    } 
 }
 
 export default InitiateFirebase
