5fc9359c41ddc72aeff4c5da946ab12cdc100b5a
João Lucas <joaolucascp16@gmail.com>
2022-07-04 15:40:19 -0300

    ManageDb.jsx: class component to export firebase functions

--- src/config/ManageDb.jsx
+++ src/config/ManageDb.jsx
@@ -1,19 +1,35 @@
 import InitiateFirebase from "./InitiateFirebase"
 // eslint-disable-next-line
 import { ref, set, get, push, onValue, off, update, remove, child } from 'firebase/database'
+// eslint-disable-next-line
+import { ref as storage_ref, uploadBytes, uploadString, uploadBytesResumable, getDownloadURL, deleteObject, listAll, list } from 'firebase/storage'
 
 class ManageDb {
-    constructor(user, currentFolder = []) {
+    constructor(user, currentFolder = [], fileName = '') {
         this.db = InitiateFirebase()
-        this.ref = ref(this.db, 'Users/' + user + '/' + currentFolder.join('/'))
+        this.storage = InitiateFirebase('storage')
+        this.ref = ref(this.db, `Users/${user}/${currentFolder.join('/')}`)
+        this.storage_ref = storage_ref(this.storage, `Users/${user}/${currentFolder.join('/')}/${fileName}`)
     }
 
     getUserRef() {
         return this.ref
     }
+
+    getStorageRef() {
+        return this.storage_ref
+    }
     
     setInUser(content = {}) {
         return set(push(this.getUserRef()), content)
+    }
+
+    startUploadBytesResumable(file) {
+        return uploadBytesResumable(this.getStorageRef(), file)
+    }
+
+    getDownload(ref) {
+        return getDownloadURL(ref)
     }
 }
 
