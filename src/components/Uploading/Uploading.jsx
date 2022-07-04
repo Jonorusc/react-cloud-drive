7aa7607250a82c9900d87017a4cacc1af1b8faff
João Lucas <joaolucascp16@gmail.com>
2022-07-04 15:39:19 -0300

    Uploading progressbar status and styles

--- src/components/Uploading/Uploading.jsx
+++ src/components/Uploading/Uploading.jsx
@@ -1,21 +1,46 @@
 import React from 'react'
+import PauseCircleRoundedIcon from '@mui/icons-material/PauseCircleRounded'
+import PlayCircleRoundedIcon from '@mui/icons-material/PlayCircleRounded'
+import CancelRoundedIcon from '@mui/icons-material/CancelRounded'
 
 import './Uploading.css'
 
-function Uploading({ title, remaining, percent }) {
-  return (
-    <div className="uploading visible">
+function Uploading({ relative, title, remaining, percent, uploadingStatus, setUploadingStatus, i }) {
+    return (
+    <div className={relative ? 'uploading active visible': 'uploading visible'}>
         <div className="left_side">
             <span className="title">{title}</span>
             <div className="uploading_progress">
                 <div className="uploading_progress_bar"
-                    style={{width: `${percent}%'`}}
+                    style={{width: `${percent}%`}}
                 >{percent}%</div>
             </div>
         </div>
         <div className="right_side">
-            <small><span className="remaining_time">{remaining}</span></small>
-            
+            <small><span className="remaining_time">{(uploadingStatus.payload === 'pause' && uploadingStatus.index === i) ? 'paused' : remaining}</span></small>
+            <div className='uploading_icons'>
+            <>
+                <div
+                    onClick={() => {setUploadingStatus({index: i, payload: 'cancel'})}}
+                >
+                    <CancelRoundedIcon className="uploading_icon" />
+                </div>
+                {uploadingStatus.payload !== 'pause' && (
+                    <div 
+                        onClick={() => {setUploadingStatus({index: i, payload: 'pause'})}}
+                    >
+                        <PauseCircleRoundedIcon className="uploading_icon" />
+                    </div>
+                )}
+                {uploadingStatus.payload === 'pause'  && (
+                    <div
+                        onClick={() => {setUploadingStatus({index: i, payload: 'resume'})}}
+                    >
+                        <PlayCircleRoundedIcon className="uploading_icon" />
+                    </div>
+                )}
+            </>
+            </div>
         </div>
     </div>
   )
