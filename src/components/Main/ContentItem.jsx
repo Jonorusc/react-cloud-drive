493d9b8a6d614e71dc65c05022fcadb513f17ac1
João Lucas <joaolucascp16@gmail.com>
2022-07-09 20:24:58 -0300

    ContentItem.jsx: improve item selection logic

--- src/components/Main/ContentItem.jsx
+++ src/components/Main/ContentItem.jsx
@@ -1,25 +1,24 @@
-import React, { useState, useRef, useEffect, useContext  } from 'react'
+import React, { useState, useRef, useContext  } from 'react'
 import DriveFileRenameOutlineRoundedIcon from '@mui/icons-material/DriveFileRenameOutlineRounded'
-import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded'
+import DeleteRoundedIcon from '@mui/icons-material/DeleteRounded'
 import DownloadRoundedIcon from '@mui/icons-material/DownloadRounded'
+import RestorePageRoundedIcon from '@mui/icons-material/RestorePageRounded'
+import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded'
 // helpers
 import useClickOutSide from '../../helpers/clickOutside'
 import UserDrive from '../../helpers/userDrive'
 import ManageDb from '../../config/ManageDb'
-import { useMediaQuery } from "react-responsive"
+// import { useMediaQuery } from "react-responsive"
 
-function ContentItem({ title, preview, item, index, active, setItemOptions }) {
+function ContentItem({ title, preview, item, index, active, setItemOptions, url }) {
     const [onContextMenu, setOnContextMenu] = useState(false),
         { userDrive, setUserDrive } = useContext(UserDrive),
-        menuRef = useRef(),
-        mobile = useMediaQuery({
-            query: "(max-width: 720px)",
-        })
+        menuRef = useRef()
+        // mobile = useMediaQuery({
+        //     query: "(max-width: 720px)",
+        // })
 
-    useEffect(() => {
-        // console.log(item)
-        // eslint-disable-next-line
-    }, [])
+    let activeTemp = userDrive?.isActive, currentFile
 
     useClickOutSide(menuRef, () => {
         if(onContextMenu)setOnContextMenu(false)        
@@ -46,12 +45,11 @@ function ContentItem({ title, preview, item, index, active, setItemOptions }) {
 
     function onClick(e) {
         // if the user is using mobile phone, we'll open the folder when 'onclick'
-        if(mobile) {
-            onDblClick(e)
-            return 
-        }
+        // if(mobile) {
+        //     onDblClick(e)
+        //     return 
+        // }
         if(e.ctrlKey) {
-            let activeTemp = userDrive?.isActive, currentFile
             if(userDrive?.isActive.indexOf(item?.key) !== -1) 
                 activeTemp.splice(userDrive?.isActive.indexOf(item?.key), 1)
              else 
@@ -98,57 +96,97 @@ function ContentItem({ title, preview, item, index, active, setItemOptions }) {
                 />
                 </div>
                 <span className="content_title">{title}</span>
-                <div className={onContextMenu ? "content_item_options active" : "content_item_options"} ref={menuRef}>
-                    <div className="blur">
-                        <div className="options">
-                            <div className="options_item" 
-                                    onClick={() => {setItemOptions({
-                                        action: 'rename',
-                                        payload: {
-                                            key: item?.key,
-                                            index,
-                                        },
-                                        name: item?.data?.name,
-                                    })
-                                    setOnContextMenu(false)
-                                }}
-                            >
-                                <DriveFileRenameOutlineRoundedIcon className='item_icon' />
-                                Rename    
-                            </div>
-                            {item?.data.type !== 'folder' && (
+                {/* check if view is content or trash */}
+                {url === 'content' ? (
+                    <div className={onContextMenu ? "content_item_options active" : "content_item_options"} ref={menuRef}>
+                        <div className="blur">
+                            <div className="options">
+                                {activeTemp?.length === 1 && (
+                                    <div className="options_item" 
+                                            onClick={() => {setItemOptions({
+                                                action: 'rename',
+                                                payload: {
+                                                    key: item?.key,
+                                                    index,
+                                                },
+                                                name: item?.data?.name,
+                                            })
+                                            setOnContextMenu(false)
+                                        }}
+                                    >
+                                        <DriveFileRenameOutlineRoundedIcon className='item_icon' />
+                                        Rename    
+                                    </div>
+                                )} 
+                                {item?.data.type !== 'folder' && activeTemp.length === 1 && (
+                                    <div className="options_item"
+                                        onClick={() => {setItemOptions({
+                                                action: 'download',
+                                                payload: item?.data?.downloadURL,
+                                                name: item?.data?.name
+                                            })
+                                            setOnContextMenu(false)
+                                        }}
+                                    >
+                                        <DownloadRoundedIcon className='item_icon' />
+                                        Download
+                                    </div>
+                                )}
                                 <div className="options_item"
                                     onClick={() => {setItemOptions({
-                                            action: 'download',
-                                            payload: item?.data?.downloadURL,
+                                            action: 'movetotrash',
+                                            payload: {
+                                                excluded: true,
+                                                keys: activeTemp,
+                                            },
                                             name: item?.data?.name
                                         })
                                         setOnContextMenu(false)
                                     }}
                                 >
-                                    <DownloadRoundedIcon className='item_icon' />
-                                    Download
+                                    <DeleteRoundedIcon className='item_icon' />
+                                    Move to trash
                                 </div>
-                            )}
-                            <div className="options_item"
-                                onClick={() => {setItemOptions({
-                                        action: 'movetotrash',
-                                        payload: {
-                                            key: item?.key,
-                                            excluded: true,
-                                            index,
-                                        },
-                                        name: item?.data?.name
-                                    })
-                                    setOnContextMenu(false)
-                                }}
-                            >
-                                <DeleteForeverRoundedIcon className='item_icon' />
-                                Move to trash
                             </div>
                         </div>
                     </div>
-                </div>
+                ) : (
+                    <div className={onContextMenu ? "content_item_options active" : "content_item_options"} ref={menuRef}>
+                        <div className="blur">
+                            <div className="options">
+                                <div className="options_item"
+                                    onClick={() => {setItemOptions({
+                                            action: 'restore',
+                                            payload: {
+                                                excluded: false,
+                                                keys: activeTemp,
+                                            },
+                                            name: item?.data?.name
+                                        })
+                                        setOnContextMenu(false)
+                                    }}
+                                >
+                                    <RestorePageRoundedIcon className='item_icon' />
+                                    Restore
+                                </div>
+                                <div className="options_item"
+                                    onClick={() => {setItemOptions({
+                                            action: 'delete',
+                                            payload: {
+                                                keys: activeTemp,
+                                            },
+                                            name: item?.data?.name
+                                        })
+                                        setOnContextMenu(false)
+                                    }}
+                                >
+                                    <DeleteForeverRoundedIcon className='item_icon' />
+                                    Delete
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     )
