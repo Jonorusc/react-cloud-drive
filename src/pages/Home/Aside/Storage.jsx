73e1ff754016103bd7f3a680aba2af4ea216a2d8
João Lucas <joaolucascp16@gmail.com>
2022-07-11 14:56:29 -0300

    starting to build storage functionality

--- src/pages/Home/Aside/Storage.jsx
+++ src/pages/Home/Aside/Storage.jsx
@@ -3,16 +3,16 @@ import StorageRoundedIcon from '@mui/icons-material/StorageRounded'
 
 import './Storage.css'
 
-function Storage( {ammout} ) {
+function Storage({ ammout, percent }) {
     return (
         <div className='storage'>
             <div className="storage_header">
-                <StorageRoundedIcon />
+                <StorageRoundedIcon className={percent >= 80 ? "storage_header_icon warning" : "storage_header_icon"}  />
                 <span className="text">Storage</span>
             </div>
             <div className="storage_body">
                 <div className="storage_progress">
-                    <div className="storage_progress_bar" style={{width: '10.3%'}}></div>
+                    <div className={percent >= 80 ? "storage_progress_bar warning" : "storage_progress_bar"} style={{width: `${percent}%`}}></div>
                 </div>
                 <span className="storage_ammount">{ammout}</span>
             </div>
