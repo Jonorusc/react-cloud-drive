16237047dd00cae58b03ac47c4a5ff9b4550efd1
João Lucas <joaolucascp16@gmail.com>
2022-07-12 19:38:14 -0300

    Activate.jsx: using .env file

--- src/pages/Activate/Activate.jsx
+++ src/pages/Activate/Activate.jsx
@@ -23,7 +23,7 @@ function Activate() {
     useEffect(() => {
         const activateAccount = async () => {
             setLoading(true)
-            const { data } =  await axios.post('http://localhost:8000/activate', 
+            const { data } =  await axios.post(`${process.env.REACT_APP_BACKEND}activate`, 
             { token: token.token }, {
                 headers: {
                     Authorization: `Bearer ${user.token}`,
