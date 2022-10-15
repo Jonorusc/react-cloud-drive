0d7ca092e2c3352e7c9c547a59dcdf11da3218f5
João Lucas <joaolucascp16@gmail.com>
2022-10-14 22:08:10 -0300

    BreadCrumbs update: leaving the code cleaner

--- src/components/Main/BreadCrumbs.jsx
+++ src/components/Main/BreadCrumbs.jsx
@@ -1,47 +1,48 @@
 import React, { useRef } from 'react'
 
-function BreadCrumbs({ userDrive, setUserDrive, url}) {
+function BreadCrumbs({ userDrive, setUserDrive, url }) {
     const navigationRef = useRef()
     let path = []
-    return (
-        <div className={url === 'trash' ? "breadcrumbs trash" : "breadcrumbs"}>
-            {userDrive?.currentFolder?.length > 1 ? (
+    const RenderJSX = () => {
+        if (userDrive?.currentFolder?.length > 1) {
+            return (
                 <div className="navigation" ref={navigationRef}>
                     {userDrive?.currentFolder.map((item, i) => {
                         path.push(item)
                         return (
                             <React.Fragment key={i}>
-                                <div className="link" data-path={path.join('/')}
+                                <div
+                                    className="link"
+                                    data-path={path.join('/')}
                                     onClick={(e) => {
                                         setUserDrive({
-                                            ...userDrive, 
-                                            currentFolder: e.target.dataset.path.split('/'), 
+                                            ...userDrive,
+                                            currentFolder: e.target.dataset.path.split('/'),
                                             currentFile: '',
                                         })
                                     }}
-                                    >
-                                    {item} 
-                                </div> 
-                                <span className='egt'>&gt;</span>
+                                >
+                                    {item}
+                                </div>
+                                <span className="egt">&gt;</span>
                             </React.Fragment>
                         )
                     })}
-                    {userDrive?.currentFile !== "" && (
-                        <span className='link_span'>
-                            {userDrive?.currentFile}
-                        </span>
-                    )}
+                    {userDrive?.currentFile !== '' && <span className="link_span">{userDrive?.currentFile}</span>}
                 </div>
-            ) : (
+            )
+        } else {
+            return (
                 <div className="navigation">
                     <div className="link">{userDrive?.currentFolder}</div>
-                    {userDrive?.currentFile !== "" && (
-                        <span className='link_span'>
-                           : {userDrive?.currentFile}
-                        </span>
-                    )}
+                    {userDrive?.currentFile !== '' && <span className="link_span">: {userDrive?.currentFile}</span>}
                 </div>
-            )}
+            )
+        }
+    }
+    return (
+        <div className={`breadcrumbs ${url === 'trash' ? 'trash' : ''}`}>
+            <RenderJSX />
         </div>
     )
 }
